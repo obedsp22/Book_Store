520d9ed6a6d0ff37c61bd81700d4539073d4b23b
obedsp22 <obedp375@gmail.com>
2024-03-04 19:26:55 -0500

    revised

---
+++ database/sample.sql
@@ -0,0 +1,115 @@
+CREATE DATABASE  IF NOT EXISTS `book_store` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `book_store`;
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: book_store
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books` (
+  `BookId` int NOT NULL AUTO_INCREMENT,
+  `Cover` varchar(200) NOT NULL,
+  `Title` varchar(200) NOT NULL,
+  `Author` varchar(200) NOT NULL,
+  `Genre` varchar(100) NOT NULL,
+  `isBorrowed` varchar(45) NOT NULL,
+  PRIMARY KEY (`BookId`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (1,'book1.jpg','Harry Potter and The Sorcerer\'s Stone','J.K. Rowling','Fiction','Borrowed'),(3,'book2.jpg','Harry Potter and The Chamber of Secrets','J.K. Rowling','Mystery','Available'),(5,'only-the-dead.jpg','Only The Dead','Jack Carr','Fiction','Available'),(6,'identity.jpg','Identity','Nora Roberts','Non-Fiction','Available'),(7,'theHungerGames.jpg','The Hunger Games','Suzzanne Collins','Fiction','Available');
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `loans`
+--
+
+DROP TABLE IF EXISTS `loans`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `loans` (
+  `LoanId` int NOT NULL AUTO_INCREMENT,
+  `LoanDate` date NOT NULL,
+  `ReturnDate` date NOT NULL,
+  `BookId` int NOT NULL,
+  `ReaderId` int NOT NULL,
+  PRIMARY KEY (`LoanId`),
+  KEY `Foreign_Key_Books_idx` (`BookId`),
+  KEY `Foreign_Key_Readers_idx` (`ReaderId`),
+  CONSTRAINT `Foreign_Key_Books` FOREIGN KEY (`BookId`) REFERENCES `books` (`BookId`) ON DELETE CASCADE,
+  CONSTRAINT `Foreign_Key_Readers` FOREIGN KEY (`ReaderId`) REFERENCES `readers` (`ReadId`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loans`
+--
+
+LOCK TABLES `loans` WRITE;
+/*!40000 ALTER TABLE `loans` DISABLE KEYS */;
+INSERT INTO `loans` VALUES (13,'2023-06-10','2023-06-10',6,3),(14,'2023-06-23','2023-06-29',6,1),(16,'2023-06-09','2023-06-15',1,1),(18,'2024-03-10','2024-03-30',1,6);
+/*!40000 ALTER TABLE `loans` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `readers`
+--
+
+DROP TABLE IF EXISTS `readers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `readers` (
+  `ReadId` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(200) NOT NULL,
+  `LastName` varchar(200) NOT NULL,
+  `Address` varchar(200) NOT NULL,
+  `Phone` varchar(45) NOT NULL,
+  PRIMARY KEY (`ReadId`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `readers`
+--
+
+LOCK TABLES `readers` WRITE;
+/*!40000 ALTER TABLE `readers` DISABLE KEYS */;
+INSERT INTO `readers` VALUES (1,'Johhny ','Cage','123 Kombat St','123-456-7890'),(3,'John','Doe','999 10th St','987-987-9788'),(6,'James','Miller','45678 Street Dr','456-789-0909');
+/*!40000 ALTER TABLE `readers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-04 19:25:20
